fd9566a1d8d52a78969f273d86d5369b2f58f4a8
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2019-12-24 13:47:08 +0100

    import the css file in index.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,19 +1,12 @@
 import React, { Component, Fragment } from 'react';
-
 class StorePicker extends Component {
     render() {
         return (
-            <Fragment>
-                <form className="store-selector">
-                    <h2>Please enter the store</h2>
-                    <input type="text" name="" id="" placeholder="Store Name" />
-                    <button type="submit">Visit Store -></button>
-                </form>
-                <p>
-                    To put Sibling elements, should wrap the elements in
-                    Fragment
-                </p>
-            </Fragment>
+            <form className="store-selector">
+                <h2>Please enter the store</h2>
+                <input type="text" name="" id="" placeholder="Store Name" />
+                <button type="submit">Visit Store -></button>
+            </form>
         );
     }
 }
