c7590832dfbf6e86dd3da848c636b90c0548d5dd
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2019-12-24 19:31:39 +0100

    add submit handle function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 class StorePicker extends Component {
+    constructor() {
+        super();
+
+        this.myInput = React.createRef();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore = event => {
+        event.preventDefault();
+        // this.ref.current
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter the store</h2>
-                <input type="text" name="" id="" placeholder="Store Name" />
+                <input
+                    type="text"
+                    required
+                    placeholder="Store Name"
+                    ref={this.myInput}
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store -></button>
             </form>
         );
