2524a1ed06d5161636a3487e1dffe94d11a5d10a
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2019-12-24 13:28:58 +0100

    add simple JSX in StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class StorePicker extends Component {
     render() {
         return (
-            <div>
-                <p>hey you!!!</p>
-            </div>
+            <Fragment>
+                <form className="store-selector">
+                    <h2>Please enter the store</h2>
+                    <input type="text" name="" id="" placeholder="Store Name" />
+                    <button type="submit">Visit Store -></button>
+                </form>
+                <p>
+                    To put Sibling elements, should wrap the elements in
+                    Fragment
+                </p>
+            </Fragment>
         );
     }
 }
