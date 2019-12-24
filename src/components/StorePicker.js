758881b0baef1d3e0932f6d98d894573790e65a7
Zheng Li <zhengli@Zhengs-MacBook-Air.local>
2019-12-24 12:29:12 +0100

    The first component StorePicker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+    render() {
+        return (
+            <div>
+                <p>hey you!!!</p>
+            </div>
+        );
+    }
+}
+
+export default StorePicker;
